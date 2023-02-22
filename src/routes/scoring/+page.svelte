43629ae939ae859db4de6902f8a1ab01e7332b0f
SUNHO LEE <rhasya@gmail.com>
2023-02-22 23:56:14 +0900

    create scoringlist page

--- src/routes/scoring/+page.svelte
+++ src/routes/scoring/+page.svelte
@@ -1,34 +1,6 @@
-<script lang="ts">
-	import { enhance } from '$app/forms';
-  // @ts-ignore
-	import FilePond, { registerPlugin } from 'svelte-filepond';
-  import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
-
-	let pond;
-	let name = 'filepond';
-	const handleInit = () => {};
-	const handleAddFile = () => {};
-
-  registerPlugin(FilePondPluginFileValidateType);
-</script>
-
 <svelte:head>
 	<title>OMR :: Scoring</title>
 </svelte:head>
-<h3>Scoring</h3>
-<form method="POST" use:enhance>
-	<input type="text" name="title" placeholder="Title" required />
-	<FilePond
-		bind:this={pond}
-		{name}
-		allowMultiple={true}
-    storeAsFile={true}
-		oninit={handleInit}
-		onaddfile={handleAddFile}
-    acceptedFileTypes={['image/jpeg']}
-	/>
-	<button>Submit</button>
-</form>
-<style>
-  
-</style>
+
+<h1>Scoring</h1>
+<a href="/scoring/new" role="button">New</a>
---
+++ src/routes/scoring/new/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	// @ts-ignore
+	import FilePond, { registerPlugin } from 'svelte-filepond';
+	import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+
+	let pond;
+	let name = 'filepond';
+	const handleInit = () => {};
+	const handleAddFile = () => {};
+
+	registerPlugin(FilePondPluginFileValidateType);
+</script>
+
+<svelte:head>
+	<title>OMR :: Scoring</title>
+</svelte:head>
+<h1>New Scoring</h1>
+<form method="POST" use:enhance>
+	<input type="text" name="title" placeholder="Title" required />
+	<FilePond
+		bind:this={pond}
+		{name}
+		allowMultiple={true}
+		storeAsFile={true}
+		oninit={handleInit}
+		onaddfile={handleAddFile}
+		acceptedFileTypes={['image/jpeg']}
+	/>
+	<div class="btn-container">
+		<button type="submit" class="submit-btn">Submit</button>
+		<a href="/scoring" class="secondary" role="button">Cancel</a>
+	</div>
+</form>
+
+<style>
+	.submit-btn {
+		width: auto;
+		margin: 0;
+	}
+	.btn-container {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-end;
+		gap: 1rem;
+	}
+</style>
