540538f24f65ddcb071d3a9df2d1124fe9b30659
SUNHO LEE <rhasya@gmail.com>
2023-02-27 01:01:01 +0900

    OMR 처리 추가

--- worker/worker.py
+++ worker/worker.py
@@ -1,5 +1,7 @@
 import os
+import subprocess
 import pika
+import json
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
@@ -16,11 +18,20 @@ channel.basic_qos(prefetch_count=1)
 def callback(ch, method, properties, body):
     print("[x] Received %r" % body.decode())
 
-    # update to database
-    db.scores.update_one({'_id': ObjectId(body.decode())}, 
-        {'$set': {'status': 'finish'}})
+    a = db.scores.find_one({"_id": ObjectId(body.decode())})
+    
+    # run omr_processor
+    print("[x] Start processing")
+    p = subprocess.run(["python", "omr_reader/main.py", f"../{a['path']}", a["type"]], capture_output=True)
+    if p.returncode == 0:
+        # update to database
+        r = json.loads(p.stdout)
+        db.scores.update_one({"_id": a["_id"]}, {"$set": {"status": "finish", "result": r}})
+    else:
+        db.scores.update_one({"_id": a["_id"]}, {"$set": {"status": "error"}})
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
+    print("[x] Finish processing")
 
 channel.basic_consume(queue=queue_name, on_message_callback=callback)
 channel.start_consuming()
