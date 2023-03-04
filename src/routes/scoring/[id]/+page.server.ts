3467ea95cb06f512f31c522e9c3e34448c039db9
SUNHO LEE <rhasya@gmail.com>
2023-03-04 21:57:30 +0900

    상세화면 표시 중 오류가 발생하는 경우 수정

--- src/routes/scoring/[id]/+page.server.ts
+++ src/routes/scoring/[id]/+page.server.ts
@@ -11,8 +11,14 @@ export const load = (async ({ params, locals }) => {
 	// processing
 	if (score == null) throw error(404, 'Not Found');
 	if (score.user.toString() !== locals.user?.userId) throw error(403, 'Unauthorize');
+
+	let answer_title = '';
+	if (score.answer && 'title' in score.answer) {
+		answer_title = score.answer.title;
+	}
+
 	if (score.type === 'type_a') {
-		const conv = score?.result?.map((o, idx) => ({
+		const result = score.result?.map((o, idx) => ({
 			...o,
 			page_no: idx + 1,
 			centre_number: `${o.centre_number_1}${o.centre_number_2}`,
@@ -24,12 +30,12 @@ export const load = (async ({ params, locals }) => {
 				title: score.title,
 				type: score.type,
 				path: score.path,
-				answer_title: (score.answer as IAnswer).title,
-				result: conv
+				answer_title,
+				result
 			}
 		};
 	} else if (score.type === 'type_b') {
-		const conv = score.result?.map((o, idx) => ({
+		const result = score.result?.map((o, idx) => ({
 			...o,
 			page_no: idx + 1,
 			given_name: makeName(o, 'given_name_'),
@@ -41,8 +47,8 @@ export const load = (async ({ params, locals }) => {
 				title: score.title,
 				type: score.type,
 				path: score.path,
-				answer_title: (score.answer as IAnswer).title,
-				result: conv
+				answer_title,
+				result
 			}
 		};
 	}
@@ -53,12 +59,13 @@ export const load = (async ({ params, locals }) => {
 }) satisfies PageServerLoad;
 
 const makeName = (o: IResult, keyHead: string) => {
-	const arr = 'abcdefghijklmnopqrstuvwxyz';
+	const str = 'abcdefghijklmnopqrstuvwxyz';
 	let name = '';
 	for (let i = 1; i <= 12; i++) {
 		let key = `${keyHead}${i}`;
-		if ((o[key] instanceof Array<number>) && (o[key] as number[]).length > 0) {
-			name += arr[(o[key] as number[])[0]];
+		const arr = o[key] as number[];
+		if (arr.length > 0) {
+			name += str[arr[0]];
 		}
 	}
 	return name;
